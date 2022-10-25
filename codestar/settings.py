a732da83821a97a32ab44c0e48262137f79acb79
Rhesmika <Rhesmika@gmail.com>
2022-10-25 20:46:29 +0000

    update local host

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rez-codestar2021.herokuapp.com', localhost]
+ALLOWED_HOSTS = ['rez-codestar2021.herokuapp.com', 'localhost']
 
 
 # Application definition
